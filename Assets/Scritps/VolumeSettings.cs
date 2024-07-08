5e50d389122405f34a03b6c043b0bf40c70d711a
MidasRookie <ejae1804@gmail.com>
2024-07-08 13:16:21 +0700

    Nambah Lobby

---
+++ Assets/Scritps/VolumeSettings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+    public void setMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume)*20); 
+    }
+}
